1e802a5a7827d6eb361a23d28fdbf5d64dd3fa42
Thomas Seaver <thomasseaver98@gmail.com>
2022-01-03 19:45:01 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const name = 'ping';
 const description = 'Replies with pong!';
 
 const execute = async (interaction) => {
-  await interaction.reply(`Pong! (Response time: ${Date.now() - interaction.createdAt})`);
+  await interaction.reply(`Pong! (Response time: ${Date.now() - interaction.createdAt} ms)`);
 };
 
 module.exports = {
